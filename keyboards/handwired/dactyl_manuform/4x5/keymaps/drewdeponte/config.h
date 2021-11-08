5357a5ab486cbe1be0f046e538e7d664fbd88b0c
Drew De Ponte <cyphactor@gmail.com>
2021-11-08 16:10:16 -0500

    Enable home row mods
    See https://precondition.github.io/home-row-mods for details. Basically
    it sets up my home row to act as the modifier keys if held and act as
    the normal letters when tapped.
    
    This definitely makes the keyboard feel lagged because it waits until
    keyup to register a keypress rather than keydown like it normally would.
    Also, there can be gotchas with with the TAPPING_TERM, how long you have
    to hold the key down for it to register as a modifier. So this will
    likely be something that has to be tweaked as my speed evolves.
    
    Also, IGNORE_MOD_TAP_INTERRUPT and TAPPING_FORCE_HOLD are critical for
    handling some of those cases. See the QMK documentation for details on
    these settings.
    
    I will have to see how I like this and if I feel like it is worth the
    delay feeling.

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/drewdeponte/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/drewdeponte/config.h
@@ -20,6 +20,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define TAPPING_TERM 175
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
 /* Use I2C or Serial, not both */
 #define USE_SERIAL
 //#define USE_I2C
